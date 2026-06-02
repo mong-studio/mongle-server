24f222d1b6edcb88119e74bd7f4d0882ae47a795
Junghee Im(Jion) <109445934+bigmooon@users.noreply.github.com>
2026-06-02 16:33:36 +0900

    fix: add todo comments to quests urls.py

--- apps/quests/urls.py
+++ apps/quests/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from apps.quests.views import QuestDetailView, QuestListView
 
+# TODO: 추후 수정 필요
 urlpatterns = [
     path("", QuestListView.as_view(), name="quest-list"),
     path("<uuid:quest_id>/", QuestDetailView.as_view(), name="quest-detail"),
