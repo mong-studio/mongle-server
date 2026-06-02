ea9e9b1ceaed7b97ff12446c1110685471a10480
Junghee Im(Jion) <109445934+bigmooon@users.noreply.github.com>
2026-06-02 16:38:31 +0900

    chore: add todo comments to todo urls.py

--- apps/todos/urls.py
+++ apps/todos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from apps.todos.views import TodoDetailView, TodoListCreateView
 
+# TODO: 추후 수정 필요
 urlpatterns = [
     path("", TodoListCreateView.as_view(), name="todo-list"),
     path("<uuid:todo_id>/", TodoDetailView.as_view(), name="todo-detail"),
